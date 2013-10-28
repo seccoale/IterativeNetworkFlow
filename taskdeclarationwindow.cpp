0cf9dd40677951b6c876cf5d69faac42d6de1c3b
seccoale <seccoale@gmail.com>
2013-10-28 15:55:01 +0100

    Added more signals/slots and corrected jobs into tasks

---
+++ taskdeclarationwindow.cpp
@@ -0,0 +1,55 @@
+#include "taskdeclarationwindow.h"
+#include "ui_taskdeclarationwindow.h"
+
+TaskDeclarationWindow::TaskDeclarationWindow(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::TaskDeclarationWindow)
+{
+    ui->setupUi(this);
+    this->edit=false;
+}
+
+TaskDeclarationWindow::TaskDeclarationWindow(QWidget *parent, Task *TaskToEdit, int row):
+    QDialog(parent),
+    ui(new Ui::TaskDeclarationWindow){
+    ui->setupUi(this);
+    this->edit=true;
+    this->row_edited=row;
+    this->ui->task_name->setText(TaskToEdit->name.c_str());
+    this->ui->deadlineSB->setValue(TaskToEdit->deadline);
+    this->ui->exTimeSB->setValue(TaskToEdit->execution_time);
+    this->ui->periodSB->setValue(TaskToEdit->period);
+}
+
+TaskDeclarationWindow::~TaskDeclarationWindow()
+{
+    delete ui;
+}
+
+void TaskDeclarationWindow::on_buttonBox_accepted()
+{
+    Task* task=new Task(ui->task_name->text().toUtf8().constData(),ui->periodSB->value(), ui->deadlineSB->value(), ui->exTimeSB->value());
+    if(edit){
+        emit replaceTask(task, this->row_edited);
+    }
+    else {
+        emit addTask(task);
+    }
+}
+
+void TaskDeclarationWindow::on_buttonBox_rejected()
+{
+    emit discardTask();
+}
+void TaskDeclarationWindow::editTask(QString* taskLiteral){
+    Task* task=new Task();
+    task=task->compile(*taskLiteral);
+    QString* taskName=new QString(task->name.c_str());
+    this->ui->task_name->setText(*taskName);
+    delete taskName;
+    this->ui->deadlineSB->setValue(task->deadline);
+    this->ui->exTimeSB->setValue(task->execution_time);
+    this->ui->periodSB->setValue(task->period);
+    delete task;
+    this->show();
+}
---
+++ taskdeclarationwindow.h
@@ -0,0 +1,36 @@
+#ifndef TASKDECLARATIONWINDOW_H
+#define TASKDECLARATIONWINDOW_H
+
+#include <QDialog>
+#include "graphview.h"
+#include "Structs.h"
+namespace Ui {
+class TaskDeclarationWindow;
+}
+using namespace inf;
+class TaskDeclarationWindow : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit TaskDeclarationWindow(QWidget *parent = 0);
+    TaskDeclarationWindow(QWidget *parent, Task* taskToEdit,int row);
+    ~TaskDeclarationWindow();
+
+signals:
+    void addTask(Task*);
+    void discardTask();
+    void replaceTask(Task*, int);
+    
+private slots:
+    void on_buttonBox_accepted();
+    void on_buttonBox_rejected();
+    void editTask(QString* taskLiteral);
+
+private:
+    Ui::TaskDeclarationWindow *ui;
+    bool edit;
+    int row_edited;
+};
+
+#endif // TASKDECLARATIONWINDOW_H
